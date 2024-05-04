da913e6df8e12181162640fd872be7ba83e97c2b
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2024-05-05 01:21:33 +0300

    fix the ui of register page

--- src/auth/Signup/Signup.jsx
+++ src/auth/Signup/Signup.jsx
@@ -71,43 +71,47 @@ function Signup (props){
     return(
         <div className="signup-container" >
          <div className="register-form-container">
-        <div className="welcome-container">
-            <h1 className="register-header">Register</h1>
-            <br/>
-            <span className="register-subheader">Let’s Sign up first for enter into JobConnect Website.</span>
-        </div>
-        {error && <div className="error-message">{error}</div>}
-        <form className="register-form" onSubmit={submitUser}>
-            <label className='userName-label'htmlFor="userName">UserName</label>
-            <input value={userName} type="text" placeholder="userName" id="userName" name="userName" onChange={(e) => setUserName(e.target.value)}></input>
-            <label className='email-label'htmlFor="email">Email</label>
-            <input  value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"  onChange={(e) => setEmail(e.target.value)}></input>
-            <label className="password-label"  htmlFor="password">Password</label>
-            <input value={password} type="password" placeholder="************" id="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
-           <div className="menu-div">
-            <label className="select-role-label" >Select Role:</label>
-            <select className="job-role-menu"  value={jobRole} onChange={(e)=> {setJobRole(e.target.value); toggleEmployerFields(e);}}>
-                <option value="">Select</option>
-                <option value="jobseeker">Jobseeker</option>
-                <option value="employeer">Employeer</option>
-            </select>
+            <div className="welcome-container">
+                <h1 className="register-header">Register</h1>
+                <br/>
+                <span className="register-subheader">Let’s Sign up first for enter into JobConnect Website.</span>
             </div>
+            {error && <div className="error-message">{error}</div>}
+            <form className="register-form" onSubmit={submitUser}>
+                <label className='userName-label'htmlFor="userName" style={{marginLeft: "5.5ch"}}>UserName</label>
+                <input className="register-input" value={userName} type="text" placeholder="userName" id="userName" name="userName" onChange={(e) => setUserName(e.target.value)}></input>
+                
+                <label className='email-label'htmlFor="email">Email</label>
+                <input className="register-input" value={email} type="email" placeholder="youremail@gmail.com" id="email" name="email"  onChange={(e) => setEmail(e.target.value)}></input>
+                
+                <label className="password-label"  htmlFor="password">Password</label>
+                <input className="register-input" value={password} type="password" placeholder="************" id="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
+            
+            <div className="menu-div">
+                <label className="select-role-label" >Select Role:</label>
+                <select className="job-role-menu"  value={jobRole} onChange={(e)=> {setJobRole(e.target.value); toggleEmployerFields(e);}}>
+                    <option value="">Select</option>
+                    <option value="jobseeker">Jobseeker</option>
+                    <option value="employeer">Employeer</option>
+                </select>
+            </div>
+
             {showEmployerFields && (
                 <>
-            <div className="company-div">
-             <label className='company-label'htmlFor="company">Company</label>
-            <input value={company} type="text" placeholder="company" id="company" name="company" onChange={(e) => setCompany(e.target.value)}></input>
-            </div>
-            <div className="industry-div"> 
-            <label className='industry-label' htmlFor="name">Industry</label>
-            <input value={industry} type="text" placeholder="industry" id="industry" name="industry" onChange={(e) => setIndustry(e.target.value)}></input>
-            </div>
+                    <div className="company-div">
+                    <label className='company-label'htmlFor="company">Company</label>
+                    <input value={company} type="text" placeholder="company" id="company" name="company" onChange={(e) => setCompany(e.target.value)}></input>
+                    </div>
+                    <div className="industry-div"> 
+                    <label className='industry-label' htmlFor="name">Industry</label>
+                    <input value={industry} type="text" placeholder="industry" id="industry" name="industry" onChange={(e) => setIndustry(e.target.value)}></input>
+                    </div>
                 </>
             )}
             <button className="register-button" type="submit">Sign up</button>
-        </form>
+            </form>
 
-        <span className="to-login-message">Have an account? <Link to="/login" className="to-login-link">Log in</Link></span>
+            <span className="to-login-message">Have an account? <Link to="/login" className="to-login-link">Log in</Link></span>
         </div>
       </div>
       
